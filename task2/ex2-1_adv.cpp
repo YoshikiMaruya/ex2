71d97d1db7015f30d24c45eef6cb07cb95d49ce8
EC2 Default User <ec2-user@ip-172-31-44-255.ap-northeast-1.compute.internal>
2020-05-07 10:24:12 +0000

    remake ver

--- task2/ex2-1_adv.cpp
+++ task2/ex2-1_adv.cpp
@@ -1,18 +1,28 @@
-#include<iostream>
-using namespace std;
-
-void dfs(string s){
-    if(s.size()==3){
-        cout<<s<<endl;
-        return;
+    #include<iostream>
+    using namespace std;
+    
+    void dfs(string s){
+        if(s.size()==3){
+            for(int i=0;i<s.size();i++){
+                for(int j=1;j<s.size();j++){
+                    if(i==j) continue;
+                         else if(s[i]!=s[j]){
+                            if(i==2){
+                                cout<<s<<endl;
+                                return;
+                            }
+                        }
+                        else return;
+                    }
+                }
+                
+            }
+                dfs(s+'a');
+                dfs(s+'d');
+                dfs(s+'g');
     }
-    for(char c='a';c<='c';c++){
-        dfs(s+c);
+    
+    int main(){
+     dfs("");
+     return 0;
     }
-    return;
-}
-
-int main(){
- dfs("");
- return 0;
-}
